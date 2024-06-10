f2c705163d074a4380c6bc67b314e6744b1474f1
Roger Rosa <rogeritael.contato@gmail.com>
2024-06-10 00:46:11 -0300

    update: recuperando jogos e comparando se o dado já existe no banco de dados

--- src/repository/ScrapperRepository.ts
+++ src/repository/ScrapperRepository.ts
@@ -1,29 +1,51 @@
 import cheerio = require('cheerio');
 import { getPageInfo } from '../utils/getPageInfo';
+import { supabase } from '../database/supabase';
+import { GameList } from '../@types/@gameList';
 
 export class ScrapperRepository {
     static async findAll(){
-        // BUSCA A URL DE TODOS OS JOGOS EM LANÇAMENTO
+    // BUSCA A URL DE TODOS OS JOGOS EM LANÇAMENTO
         // recupera o HTML da página
         const data = await getPageInfo('https://www.ign.com/upcoming/games')
     
         // buscando informações na página
         const $ = cheerio.load(data)
-        let gameList: any = []
-
-        await $('.figure-tile').map((index, element) => {
+        let gameList: GameList[] = []
+        let gamesAlreadyAdded = await supabase.from('games').select("*")
+        
+        $('.figure-tile').map((index, element) => {
             let game_url = $(element).find('.tile-link').attr('href')
-
-            game_url &&
-                this.findByUrl(game_url)
-
-            gameList = [...gameList, 'https://www.ign.com'+game_url]
+            let game_name = $(element).find('.details figcaption').text()
+            
+            // se o jogo não existir no banco de dados, adicionamos ao gameList
+            if(gamesAlreadyAdded.data?.find((game) => game.name === game_name) === undefined){
+                gameList = [...gameList, { name: game_name, url: 'https://www.ign.com'+game_url }]
+            }
         })
+            
+        // BUSCA A INFORMAÇÃO DE CADA JOGO NA LISTA DE JOGOS QUE NÃO ESTÃO CADASTRADOS
+        for (let index = 0; index < gameList.length; index++) {
+            const currentGame = gameList[index]
 
-        // BUSCA A INFORMAÇÃO DE CADA JOGO NA LISTA SALVA
-        gameList.map((url: any) => {
-            console.log('jogo: '+url)
-        })
+            const data = await getPageInfo(currentGame.url)
+            // buscando informações na página
+            const $ = cheerio.load(data)
+            
+            const name = $('.display-title').text()
+            const dev = $('a.Developers').text()
+            const image = $('.object-thumbnail figure img').attr('src')
+            const release = $('.subtitle time').text()
+            const platforms = '*'
+
+            
+            console.log({
+                name: name, developer: dev, image: image, release: release, platforms: platforms
+            })
+            //salva o jogo no banco de dados
+        }
+
+
 
         return gameList
     }
